0dcd4efef329c523c900847d343d1af763c1bf43
otakakot <kotaro.otaka@gmail.com>
2025-09-21 16:53:50 +0900

    first commit

--- go.mod
+++ go.mod
@@ -1,8 +1,5 @@
 module github.com/otakakot/playground-cloudflare
 
-go 1.21.6
+go 1.25.1
 
-require (
-	github.com/lib/pq v1.10.9
-	github.com/syumai/workers v0.22.0
-)
+require github.com/syumai/workers v0.22.0
--- internal/function/health.go
+++ internal/function/health.go
@@ -1,40 +1,9 @@
 package function
 
 import (
-	"context"
-	"database/sql"
 	"net/http"
-	"os"
-
-	_ "github.com/lib/pq"
 )
 
 func Health(w http.ResponseWriter, req *http.Request) {
-	if _, err := GetDB(req.Context(), GetDSN()); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
-		return
-	}
-
 	w.Write([]byte("OK"))
-}
-
-func GetDSN() string {
-	return "user=" + os.Getenv("POSTGRES_USER") + " password=" + os.Getenv("POSTGRES_PASSWORD") + " host=" + os.Getenv("POSTGRES_HOST") + " port=" + os.Getenv("POSTGRES_PORT") + " dbname=" + os.Getenv("POSTGRES_DATABASE")
-}
-
-func GetDB(
-	ctx context.Context,
-	dsn string,
-) (*sql.DB, error) {
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		return nil, err
-	}
-
-	if db.Ping() != nil {
-		return nil, err
-	}
-
-	return db, nil
 }
